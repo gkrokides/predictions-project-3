ba855e45c03ccbc63c4db4bce2aef25e5442706a
George <georgekrokides@hotmail.com>
2016-10-09 15:25:56 +0300

    changed production.py settings to use mysql database instead of sqlite

--- predictions_project/settings/production.py
+++ predictions_project/settings/production.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'predictions_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'betkrok$betkrokdb',
+        'USER': 'betkrok',
+        'PASSWORD': 'thebigshipisintheport',
+        'HOST': 'betkrok.mysql.pythonanywhere-services.com',
     }
 }
 
