8a2efbd4dfea62da56b5b125823a87ac163a1961
George <georgekrokides@hotmail.com>
2016-10-09 20:05:33 +0300

    made things as they were before

--- predictions/migrations/0052_auto_20160124_1844.py
+++ predictions/migrations/0052_auto_20160124_1844.py
@@ -6,31 +6,31 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
 
-    # dependencies = [
-    #     ('predictions', '0051_points'),
-    # ]
-    #
-    # operations = [
-    #     migrations.RemoveField(
-    #         model_name='points',
-    #         name='game',
-    #     ),
-    #     migrations.RemoveField(
-    #         model_name='points',
-    #         name='team',
-    #     ),
-    #     migrations.RemoveField(
-    #         model_name='team',
-    #         name='id',
-    #     ),
-    #     migrations.AlterField(
-    #         model_name='team',
-    #         name='name',
-    #         field=models.CharField(max_length=250, serialize=False, primary_key=True),
-    #     ),
-    #     migrations.DeleteModel(
-    #         name='Points',
-    #     ),
-    # ]
+    dependencies = [
+        ('predictions', '0051_points'),
+    ]
 
-    pass
+    operations = [
+        migrations.RemoveField(
+            model_name='points',
+            name='game',
+        ),
+        migrations.RemoveField(
+            model_name='points',
+            name='team',
+        ),
+        migrations.RemoveField(
+            model_name='team',
+            name='id',
+        ),
+        migrations.AlterField(
+            model_name='team',
+            name='name',
+            field=models.CharField(max_length=250, serialize=False, primary_key=True),
+        ),
+        migrations.DeleteModel(
+            name='Points',
+        ),
+    ]
+
+
