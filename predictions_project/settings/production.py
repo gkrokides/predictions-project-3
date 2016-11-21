4a4c2cdcf2ca1f2b5d6608aa918ccbfa131c5ca6
George <georgekrokides@hotmail.com>
2016-11-21 12:55:19 +0200

    added environment vars

--- predictions_project/settings/production.py
+++ predictions_project/settings/production.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#y!0)b&a9f+*ed5xz%xfr^rqw1kqgxw+5n__z=q_teugdm!q**'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
