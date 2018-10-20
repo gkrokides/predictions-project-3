78b38632050872aec3f57abf568a447e96f94da5
George Krokides <georgekrokides@hotmail.com>
2018-10-20 10:21:13 +0300

    attempt9 to fix email

--- predictions_project/settings/production.py
+++ predictions_project/settings/production.py
@@ -153,10 +153,10 @@ SECURE_SSL_REDIRECT = True
 # # EMAIL_USE_TLS = True
 # EMAIL_USE_SSL = True
 
-EMAIL_BACKEND = 'django_smtp_ssl.SSLEmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'imap.gmail.com'
 EMAIL_HOST_USER = 'georgekrokides@gmail.com'
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS', '')
 EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-EMAIL_USE_SSL = True
+EMAIL_USE_TLS = True
+# EMAIL_USE_SSL = True
