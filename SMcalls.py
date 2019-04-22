ccdd2abd75941bc3546d16f0a5a4821577ef50dc
George Krokides <georgekrokides@hotmail.com>
2019-04-22 21:19:54 +0300

    changed the sportmonks fixture call to get the data in Europe/Athens timezone

--- SMcalls.py
+++ SMcalls.py
@@ -379,7 +379,7 @@ def SMcall_LeagueFixturesByDaterange_paginated(league, season, start_date, end_d
 
     for j in range(1, total_pages + 1):
         http5 = "&page=" + str(j)
-        requestString = http1 + start_date + '/' + end_date + http2 + api_token + http3 + http4 + http5
+        requestString = http1 + start_date + '/' + end_date + http2 + api_token + http3 + http4 + http5 + timezone
 
         response = requests.get(requestString)
         smData = response.json()
